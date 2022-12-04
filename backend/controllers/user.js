2f758cc56829a4aa366c1efa9ce43d895616637d
Rushane WIlson <rushwilson1@gmail.com>
2022-12-04 16:46:01 -0500

    Added Login Page

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,6 +5,8 @@ const {
 } = require("../helpers/validation");
 const user = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const user = require("../models/user");
 
 exports.register = async (req, res) => {
   try {
@@ -72,8 +74,71 @@ exports.register = async (req, res) => {
       { id: user._id.toString() },
       "30m"
     );
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, "7d");
 
-    res.json(user);
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message:
+        "Registration sucess! Please activate your email to complete the process",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await user.findById(user.id);
+    if (check.verified == true) {
+      return res.status(400).json({ message: "This email already activated" });
+    } else {
+      await user.findByIdAndUpdate(user.id, { verified: true });
+      return res
+        .status(200)
+        .json({ message: "Account has been activated successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await user.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message:
+          "The email address you entered is not connecting to an account.",
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: "Invalid credentials. Please try again.",
+      });
+    }
+    const token = generateToken({ id: user._id.toString() }, "7d");
+
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message:
+        "Registration sucess! Please activate your email to complete the process",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -8,8 +8,8 @@ const { EMAIL, MAILING_ID, MAILING_REFRESH, MAILING_SECRET } = process.env;
 
 const auth = new OAuth2(
   MAILING_ID,
-  MAILING_REFRESH,
   MAILING_SECRET,
+  MAILING_REFRESH,
   oauth_link
 );
 
@@ -33,7 +33,7 @@ exports.setVerificationEmail = (email, name, url) => {
     from: EMAIL,
     to: email,
     subject: "Facebook email verification",
-    html: ``,
+    html: `<div style="max-width:700px;margin-bottom:1rem;display:flex;align-items:center;gap:10px;font-family:Roboto;font-weight:600;color:#3b5998"><img src="res.cloudinary.com/dmhcnhtng/image/upload/v1645134414/logo_cs1si5.png"><span>Action required: Activate your FacebookClone account</span></div><div style="padding:1rem 0;border-top:1px solid #e5e5e5;border-bottom:1px solid #e5e5e5;color:#141823;font-size:17px;font-family:Roboto"><span>Hello ${name}</span><div style="padding:20px 0"><span style="padding:1.5rem 0">You recently create a FacebookClone account. To complete your registration, please confirm your account.</span></div><div><a href=${url} style="width:200px;padding:10px 15px;background:#4c649b;color:#fff;text-decoration:none;font-weight:600">Confirm Your Account</a></div><br><div style="padding-top:20px"><span style="margin:1.5rem 0;color:898f9c">FacebookClone allows you to stay in touch with all your friemds, once registered on FacebookClone, you can share photos, organize events and much more</span></div></div>`,
   };
   stmp.sendMail(mailOptions, (err, res) => {
     if (err) return err;
