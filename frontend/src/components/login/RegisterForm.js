c4fd88872034727a2e544b0852315e7f24380554
Rushane WIlson <rushwilson1@gmail.com>
2022-12-05 08:28:10 -0500

    Stored Cookies Per User

---
+++ frontend/src/components/login/RegisterForm.js
@@ -0,0 +1,227 @@
+import { Form, Formik } from "formik";
+import { useState } from "react";
+import * as Yup from "yup";
+import RegisterInput from "../inputs/registerInput";
+import DateOfBirthSelect from "./DateOfBirthSelect";
+import GenderSelect from "./GenderSelect";
+import ClipLoader from "react-spinners/ClipLoader";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+export default function RegisterForm() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userInfos = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth(),
+    bDay: new Date().getDate(),
+    gender: "",
+  };
+
+  const [user, setUser] = useState(userInfos);
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+  const yearTemp = new Date().getFullYear();
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const years = Array.from(new Array(108), (val, index) => yearTemp - index);
+  const months = Array.from(new Array(12), (val, index) => 1 + index);
+  const getDays = () => {
+    return new Date(bYear, bMonth, 0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (val, index) => 1 + index);
+
+  const registerValidation = Yup.object({
+    first_name: Yup.string()
+      .required("What's your first name?")
+      .min(2, "First name must be between 2 and 16 characters.")
+      .max(16, "First name must be between 2 and 16 characters.")
+      .matches(
+        /^[aA-zZ\s]+$/,
+        "Numbers and special characters are not allowed"
+      ),
+    email: Yup.string()
+      .required("You'll need this to receive updates about your account.")
+      .email("Enter a valid email address."),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers, letters and special characters"
+      )
+      .min(6, "Password must be at least 6 characters")
+      .max(36, "Password cannot be over 36 characters"),
+  });
+  const [dateError, setDateError] = useState("");
+  const [genderError, setGenderError] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState("");
+
+  const registerSubmit = async () => {
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/register`,
+        {
+          first_name,
+          last_name,
+          email,
+          password,
+          bYear,
+          bMonth,
+          bDay,
+          gender,
+        }
+      );
+      setError("");
+      setSuccess(data.message);
+      const { message, ...rest } = data;
+      setTimeout(() => {
+        dispatch({ type: "LOGIN", payload: rest });
+        Cookies.set("user", JSON.stringify(rest));
+        navigate("/");
+      }, 2000);
+    } catch (error) {
+      setLoading(false);
+      setSuccess("");
+      setError(error.response.data.message);
+    }
+  };
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon"> </i>
+          <span>Sign Up</span>
+          <span>It's quick and easy!</span>
+        </div>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
+          validationSchema={registerValidation}
+          onSubmit={() => {
+            let current_date = new Date();
+            let picked_date = new Date(bYear, bMonth - 1, bDay);
+            let atLeast14 = new Date(1970 + 14, 0, 1);
+            let noMoreThan70 = new Date(1970 + 70, 0, 1);
+            if (current_date - picked_date < atLeast14) {
+              setDateError(
+                "It is recommended that you use your real date of birth"
+              );
+            } else if (current_date - picked_date > noMoreThan70) {
+              setDateError(
+                "It is recommended that you use your real date of birth"
+              );
+            } else if (gender === "") {
+              setGenderError("Please choose a gender");
+            } else {
+              setDateError("");
+              setGenderError("");
+              registerSubmit();
+            }
+          }}
+        >
+          {(formik) => (
+            <Form className="register_form">
+              <div className="reg_line">
+                <div>
+                  <RegisterInput
+                    type="text"
+                    placeholder="First name"
+                    name="first_name"
+                    onChange={handleRegisterChange}
+                  />
+                </div>
+                <div>
+                  <RegisterInput
+                    type="text"
+                    placeholder="Last name"
+                    name="last_name"
+                    onChange={handleRegisterChange}
+                  />
+                </div>
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="Email or Phone number"
+                  name="email"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="New Pasword"
+                  name="password"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Date of Birth <i className="info_icon"></i>
+                </div>
+                <DateOfBirthSelect
+                  bDay={bDay}
+                  bMonth={bMonth}
+                  bYear={bYear}
+                  days={days}
+                  months={months}
+                  years={years}
+                  handleRegisterChange={handleRegisterChange}
+                  dateError={dateError}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Gender <i className="info_icon"></i>
+                </div>
+                <GenderSelect
+                  handleRegisterChange={handleRegisterChange}
+                  genderError={genderError}
+                />
+              </div>
+              <div className="reg_infos">
+                By clicking Sign Up, you agree to our{" "}
+                <span>Terms, Data Policy &nbsp;</span>
+                and <span>Cookie Policy.</span> You may receive SMS
+                notifications from us and can opt out at any time.
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn open_signup">Sign Up</button>
+              </div>
+              <ClipLoader color="#1876f2" loading={loading} size={30} />
+              {error && <div className="error_text">{error}</div>}
+              {success && <div className="success_text">{success}</div>}
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
